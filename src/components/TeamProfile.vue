11719a252291e1f5f6e8acce0a3357adaadb4951
bharathki08 <ibharathkumar520@hotmail.com>
2019-03-09 17:03:15 -0500

    update

--- src/components/TeamProfile.vue
+++ src/components/TeamProfile.vue
@@ -1,5 +1,10 @@
 <template>
-  <p>Team Profile</p>
+ <div>
+  <p>Team Profile..</p>
+  <form>
+  <input type="text" name="search" placeholder="Search..">
+</form>
+ </div>
 </template>
 
 <script>
@@ -9,6 +14,25 @@ export default {
 </script>
 
 <style>
+input[type=text] {
+  width: 130px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background-color: white;
+  background-image: url('https://cdn1.iconfinder.com/data/icons/MetroStation-PNG/252/MB__search.png');
+  background-size: 4%;
+  background-position: right; 
+  background-repeat: no-repeat;
+  padding: 12px 20px 12px 40px;
+  -webkit-transition: width 0.3s ease-in-out;
+  transition: width 0.3s ease-in-out;
+}
+
+input[type=text]:focus {
+  width: 50%;
+}
 </style>
 
 
